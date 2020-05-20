b24e0943a1961d07d2dbb8a89aff309263ebd437
Fabio Massamitsu Minamikawa <massamitsu@msn.com>
2020-05-20 16:26:45 -0300

    Adicionar arquivos de projeto.

---
+++ AVISA/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace BenefitCsBdd
+{
+    public static class WebApiConfig
+    {
+        public static List<string> WellknownSites = new List<string>();
+
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            
+            config.Routes.MapHttpRoute(
+                name: "MedicamentoApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+        }
+    }
+}
---
+++ AVISA/IBenefit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BenefitCsBdd
+{
+    public interface IBenefit
+    {
+        IEnumerable<Deductible> GetDeductibles(string productId);
+        OopMax GetOopMax(string productId);
+        decimal GetOopMaxMet(string memberId);
+    }
+}
